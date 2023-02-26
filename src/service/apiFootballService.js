90efa992fe8e9d62440472ebd333fbee408925f7
mechief <mechiefmin@gmail.com>
2023-02-26 13:15:32 +0900

    Fix: service_leagues - 객체에서 Map 으로 타입 변경, 리그 순서를 정하기 위함

--- src/service/apiFootballService.js
+++ src/service/apiFootballService.js
@@ -77,123 +77,123 @@ export const FIXTURE_STATUS = {
   },    
 }
 
-export const SERVICE_LEAGUES = {
+export const SERVICE_LEAGUES = new Map([
   // 리그
-  '39': {
+  [39, {
     name: 'Premier League',
     nameKr: '프리미어 리그',
     type: 'league',
-  },
-  '61': {
+  }],
+  [61, {
     name: 'Ligue 1',
     nameKr: '리그 앙',
     type: 'league',
-  },
-  '78': {
+  }],
+  [78, {
     name: 'Bundesliga',
     nameKr: '분데스리가',
     type: 'league',
-  },
-  '135': {
+  }],
+  [135, {
     name: 'Serie A',
     nameKr: '세리에 A',
     type: 'league',
-  },
-  '140': {
+  }],
+  [140, {
     name: 'La Liga',
     nameKr: '라 리가',
     type: 'league',
-  },
+  }],
   // 유럽대항전
-  '2': {
+  [2, {
     name: 'UEFA Champions League',
     nameKr: 'UEFA 챔피언스 리그',
     nameShort: 'UCL',
-    type: 'cup',
-  },
-  '3': {
+    type: 'continental',
+  }],
+  [3, {
     name: 'UEFA Europa League',
     nameKr: '유로파 리그',
     nameShort: 'UEL',
-    type: 'cup',
-  },
-  '531': {
-    name: 'UEFA Super Cup',
-    nameKr: 'UEFA 슈퍼컵',
-    type: 'cup',
-  },
-  '848': {
+    type: 'continental',
+  }],
+  [848, {
     name: 'UEFA Europa Conference League',
     nameKr: '유로파 컨퍼런스 리그',
     nameShort: 'UECL',
+    type: 'continental',
+  }],
+  [531, {
+    name: 'UEFA Super Cup',
+    nameKr: 'UEFA 슈퍼컵',
     type: 'cup',
-  },
+  }],
   // 잉글랜드 컵
-  '45': {
+  [45, {
     name: 'FA Cup',
     nameKr: 'FA컵',
     type: 'cup',
-  },
-  '48': {
+  }],
+  [48, {
     name: 'League Cup',
     nameKr: '잉글랜드 리그 컵',
     type: 'cup',
-  },
-  '528': {
+  }],
+  [528, {
     name: 'Community Shield',
     nameKr: '커뮤니티 쉴드',
     type: 'cup',
-  },
+  }],
   // 프랑스 컵
-  '65': {
+  [65, {
     name: 'Coupe de la Ligue',
     nameKr: '프랑스 리그 컵',
     type: 'cup',
-  },
-  '66': {
+  }],
+  [66, {
     name: 'Coupe de France',
     nameKr: '쿠프 드 프랑스',
     type: 'cup',
-  },
-  '526': {
+  }],
+  [526, {
     name: 'Trophée des champions',
     nameKr: '트로페 데 샹피옹',
     type: 'cup',
-  },
+  }],
   // 스페인 컵
-  '143': {
+  [143, {
     name: 'Copa del Rey',
     nameKr: '코파 델 레이',
     type: 'cup',
-  },
-  '556': {
+  }],
+  [556, {
     name: 'Super Cup',
     nameKr: '스페인 슈퍼컵',
     type: 'cup',
-  },
+  }],
   // 이탈리아 컵
-  '137': {
+  [137, {
     name: 'Coppa Italia',
     nameKr: '코파 이탈리아',
     type: 'cup',
-  },
-  '547': {
+  }],
+  [547, {
     name: 'Super Cup',
     nameKr: '이탈리아 슈퍼컵',
     type: 'cup',
-  },
+  }],
   // 독일 컵
-  '81': {
+  [81, {
     name: 'DFB Pokal',
     nameKr: 'DFB 포칼컵',
     type: 'cup',
-  },
-  '529': {
+  }],
+  [529, {
     name: 'Super Cup',
     nameKr: '독일 슈퍼컵',
     type: 'cup',
-  },
-}
+  }],
+])
 
 // 승격, 강등, 유럽대항전 규칙
 export const LEAGUE_RULES = {
@@ -299,52 +299,45 @@ export const LEAGUE_RULES = {
 }
 
 export const isServiceLeague = (leagueId) => {
-  const id = typeof leagueId === 'number' ? leagueId.toString() : leagueId;
-
-  return SERVICE_LEAGUES.hasOwnProperty(id);
+  return SERVICE_LEAGUES.has(+leagueId);
 }
 
 export const getLeagueNameKr = (leagueId) => {
-  const id = typeof leagueId === 'number' ? leagueId.toString() : leagueId;
-
-  if (!isServiceLeague(id)) {
+  if (!isServiceLeague(leagueId)) {
     return null;
   }
-
-  return SERVICE_LEAGUES[id].nameKr;
+  return SERVICE_LEAGUES.get(+leagueId).nameKr;
 }
 
 export const getLeagueNameForList = (leagueId) => {
-  const id = typeof leagueId === 'number' ? leagueId.toString() : leagueId;
-
-  if (!isServiceLeague(id)) {
+  if (!isServiceLeague(leagueId)) {
     return null;
   }
 
-  return SERVICE_LEAGUES[id].nameShort || SERVICE_LEAGUES[id].nameKr;
+  return SERVICE_LEAGUES.get(+leagueId).nameShort || SERVICE_LEAGUES.get(+leagueId).nameKr;
 }
 
-export const getServiceLeagueIds = ({type, country}) => {
+export const getServiceLeagueIds = ({type, country} = {}) => {
   if (type && country) {
-    return Object.keys(SERVICE_LEAGUES).filter(leagueId => {
-      return SERVICE_LEAGUES[leagueId].type === type
-        && SERVICE_LEAGUES[leagueId].country === country;
+    return Array.from(SERVICE_LEAGUES.keys()).filter(leagueId => {
+      return SERVICE_LEAGUES.get(leagueId).type === type
+        && SERVICE_LEAGUES.get(leagueId).country === country;
     });
   } else if (type) {
-    return Object.keys(SERVICE_LEAGUES).filter(leagueId => {
-      return SERVICE_LEAGUES[leagueId].type === type;
+    return Array.from(SERVICE_LEAGUES.keys()).filter(leagueId => {
+      return SERVICE_LEAGUES.get(leagueId).type === type;
     });
   } else if (country) {
-    return Object.keys(SERVICE_LEAGUES).filter(leagueId => {
-      return SERVICE_LEAGUES[leagueId].country === country;
+    return Array.from(SERVICE_LEAGUES.keys()).filter(leagueId => {
+      return SERVICE_LEAGUES.get(leagueId).country === country;
     });
   }
 
-  return Object.keys(SERVICE_LEAGUES);
+  return Array.from(SERVICE_LEAGUES.keys());
 }
 
 export const getLeagueRule = (leagueId) => {
-  const id = typeof leagueId === 'number' ? leagueId.toString() : leagueId;
+  const id = ''+leagueId;
 
   if (!isServiceLeague(id)) {
     return null;
