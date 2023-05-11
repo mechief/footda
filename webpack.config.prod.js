ff6723f4f3360858fadb4de410eed6d6293cd332
mechief <mechiefmin@gmail.com>
2023-05-11 11:10:25 +0900

    Update: webpack.config.js dev, prod 분리

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  name: 'Footda-setting',
+  mode: 'production',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  entry: {
+    app: [
+      './src/client',
+    ]
+  }, // 입력
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 3% in KR', 'last 2 chrome versions', 'not dead'], // browserslist
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+      }
+    }],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist',
+  }, // 출력
+};
