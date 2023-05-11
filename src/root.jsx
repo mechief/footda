a4574e224db596456cb7c3f0e1850d8c5ca353be
mechief <mechiefmin@gmail.com>
2023-05-11 10:24:13 +0900

    Fix: import 파일명 오타 수정

--- src/root.jsx
+++ src/root.jsx
@@ -7,7 +7,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 import { store, persistor } from './store/store';
 
-import SiteWrapper from './components/layout/SiteWrapper';
+import SiteWrapper from './components/layout/siteWrapper';
 import SiteHeader from './components/layout/siteHeader';
 import SiteContents from './components/layout/siteContents';
 import SiteFooter from './components/layout/siteFooter';
