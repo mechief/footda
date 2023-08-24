2c9f0cdcce90680fa5186c27a8c6e9adcd3ac030
mechief <101706997+mechief@users.noreply.github.com>
2023-08-24 15:46:11 +0900

    Update: season - 유럽 리그 시즌 변경 (#1)

--- src/constants/season.js
+++ src/constants/season.js
@@ -1,5 +1,5 @@
 export const SEASON_BASIC = 2023;
-export const SEASON_EUROPE = 2022;
+export const SEASON_EUROPE = 2023;
 export const SEASON_EUROPE_CONTINENTAL = 2023;
 export const SEASON_ASIA = 2023;
 export const SEASON_ASIA_CONTINENTAL = 2023;
